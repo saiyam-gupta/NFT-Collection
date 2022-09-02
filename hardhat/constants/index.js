46040056be1f76e0bf77cb858cea02ea67d2dfd2
Saiyam Gupta <saiyamgupta67545@gmail.com>
2022-09-02 15:33:33 +0530

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xD52A3862FcCe6AEE9909afaC2fE9B074A2e78782";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// 0x1E37Cd300F7792a146F41DDcaaBB3F922Af2f18c
